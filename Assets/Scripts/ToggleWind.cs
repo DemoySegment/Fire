449a2a02934ab107f5ed8bf7de2f83a325b47520
DemoySegment <zhangfengze2020@gmail.com>
2023-10-16 14:58:33 -0400

    add random wind

--- Assets/Scripts/ToggleWind.cs
+++ Assets/Scripts/ToggleWind.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class ToggleWind : MonoBehaviour
 {
     public GameObject windZone;
+    public bool TimerOn = false;
 
     void Start()
     {
+        
         windZone.SetActive(false);
     }
-
+    private void Update()
+    {
+        if (!TimerOn)
+        {
+            windZone.transform.Rotate(new Vector3(Random.Range(-60, 60), 0, 0), Space.Self);
+            TimerOn = true;
+            StartCoroutine(timer());
+        }
+        
+    }
     public void windToggle()
     {
         if (windZone.activeSelf)
         {
             windZone.SetActive(false);
+            StopCoroutine(timer());
+            TimerOn = false;
         }
         else
         {
             windZone.SetActive(true);
+            windZone.transform.rotation = Quaternion.Euler(new Vector3(0,90,0));
+            TimerOn = false;
         }
+    }
+    IEnumerator timer()
+    {
+
+        yield return new WaitForSeconds(3);
+
+        TimerOn= false;
     }
 }
